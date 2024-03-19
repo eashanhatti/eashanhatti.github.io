27380963c9d88a3bbcdb0791113846f92dd38279
Eashan Hatti <eashanhatti@gmail.com>
2024-03-19 15:27:12 -0400

    Fix style.css path

--- build.py
+++ build.py
@@ -1,11 +1,13 @@
 import os
 
-root = os.getcwd()
 for path, _, files in os.walk('.'):
+    top = '.\\'
+    for _ in range(path.count('\\')):
+        top += '..\\'
     for file in files:
         if '.html' in file:
-            os.system(f'rm {path}/{file}')
+            os.system(f'rm {path}\\{file}')
         elif f'.md' in file:
             name, _ = os.path.splitext(file)
             if 'DRAFT' in name: continue
-            os.system(f'pandoc {path}/{name}.md -s --css {root}/style.css --mathjax -o {path}/{name}.html')
+            os.system(f'pandoc {path}\\{name}.md -s --css {top}\\style.css --mathjax -o {path}\\{name}.html')
